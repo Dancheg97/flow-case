99f0d5a67a962a77de3deef8e0b4f336f8d472f7
Danila Fomin <dangdancheg@gmail.com>
2022-07-02 12:26:41 +0300

    added new dependency and imported it

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,29 @@
 import * as vscode from "vscode";
+import {
+  camelCase,
+  capitalCase,
+  constantCase,
+  dotCase,
+  headerCase,
+  noCase,
+  paramCase,
+  pascalCase,
+  pathCase,
+  sentenceCase,
+  snakeCase,
+} from "change-case";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "flow-case.transform",
-    () => {
-      vscode.window.showInformationMessage("Hello World from flow-case!");
+  let cmd = vscode.commands.registerCommand("flow-case.transform", () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor === undefined) {
+      return;
     }
-  );
+    const text = editor.document.getText(editor.selection);
 
-  context.subscriptions.push(disposable);
+  });
+
+  context.subscriptions.push(cmd);
 }
 
 export function deactivate() {}
